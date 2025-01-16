4ff32d9bb9dcfa63791479ec1ca52545ed097d9e
MaorAmar <101297371+MaorAmar@users.noreply.github.com>
2025-01-16 18:33:50 +0200

    missingAndRepeating new changes

--- array/missingAndRepeating.js
+++ array/missingAndRepeating.js
@@ -1,19 +1,18 @@
 function missingAndRepeating(arr) {
     const obj = {};
-    let a = -1;
-    let b = -1;
     let sum = 0;
     let n = arr.length
-    for (let i = 0 ;i < n ; i++){
-        if(obj.hasOwnProperty(arr[i])){
-           b = arr[i];
+    let b = -1;
+    for (const num of arr){
+        if(obj.hasOwnProperty(num)){
+           b = num;
         }
         else {
-            obj[arr[i]] = true;
-            sum += arr[i]
+            obj[num] = true;
+            sum += num
         }
     }
-    a = n*(n+1)/2 - sum;
+    const a = n*(n+1)/2 - sum;
     return [b , a]
 }
 module.exports.missingAndRepeating = missingAndRepeating;
