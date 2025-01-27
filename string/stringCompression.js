c1a17f33d825df75fcbd16e9c20dd234fa38a22f
MaorAmar <101297371+MaorAmar@users.noreply.github.com>
2025-01-27 18:11:10 +0200

    stringCompression sol

---
+++ string/stringCompression.js
@@ -0,0 +1,46 @@
+function stringCompression(chars) {
+    const obj = {};
+    let result = [];
+    for (const char of chars){
+        if(!obj.hasOwnProperty(char)) obj[char] = 1;
+        else obj[char]++;
+    }
+    for(const [key, val] of Object.entries(obj)){
+        if(val === 1) result.push(key.toString());
+        else if(val < 10) result.push(key.toString(),val.toString());
+        else{
+            result.push(key.toString());
+            const newVal = val.toString().split("");
+            result.push(...newVal);
+        }
+    }
+    return result;
+}
+
+function stringCompressionInPlace(chars) {
+    let counter = 0;
+    for(let i = chars.length - 1;i >= 0;i--){
+        if(counter === 0) {
+            if (chars[i] !== chars[i - 1]) continue;
+            else counter++;
+        }
+        else {
+            if(chars[i] === chars[i - 1]) counter++;
+            else {
+                counter++;
+                if(counter >= 10){
+                    const counterArr = counter.toString().split("");
+                    chars.splice(i + 1,counter - 1, ...counterArr);
+                }
+                else{
+                    chars.splice(i + 1,counter - 1,counter.toString());
+                }
+                counter = 0;
+            }
+        }
+    }
+    return chars;
+}
+
+module.exports.stringCompression = stringCompression;
+module.exports.stringCompressionInPlace = stringCompressionInPlace
