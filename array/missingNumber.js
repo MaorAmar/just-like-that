d5bae94eda293068cb4f61b09406d9c9c725c652
MaorAmar <101297371+MaorAmar@users.noreply.github.com>
2025-02-09 17:42:10 +0200

    missing num changes

--- array/missingNumber.js
+++ array/missingNumber.js
@@ -8,14 +8,5 @@ function missingNumber(arr){
     }
     return 1;
 }
-function missingNumberBeter(arr){
-    arr = arr.filter(num => num > 0);
-    arr = arr.filter(num => num <= arr.length);
-    console.log(arr);
-    for(const i in arr){
-        if(arr[i] >= arr.length + 1) arr[i] = -1;
 
-    }
-    return 1;
-}
-console.log(missingNumberBeter([1,2,3,4,5,6,7,8]));
+console.log(missingNumber([1,2,3,4,5,6,7,8]));
